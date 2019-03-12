72dba712b614ee184d2f5c18437adb7e1181e4e6
dss <vladislavsidl@gmail.com>
2019-03-12 22:11:44 +0300

    epytt

--- src/JavaSweeper.java
+++ src/JavaSweeper.java
@@ -20,9 +20,6 @@ public class JavaSweeper extends JFrame
     private final int BOMBS = 10; // количество бомб
     private final int IMAGE_SIZE = 30; // размер картинки одинаковый по x и по y
 
-
-
-
     public static void main(String[] args)
     {
         new JavaSweeper();
